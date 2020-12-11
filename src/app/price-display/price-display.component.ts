06b9ba0b289be808b4abb6ecdf75245667334f54
mohit5650 <46513332+mohit5650@users.noreply.github.com>
2020-12-11 20:33:57 +0530

    files addded

---
+++ src/app/price-display/price-display.component.ts
@@ -0,0 +1,11 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-price-display',
+  template: `
+    <div class="price-display">\${{ price }}</div>
+  `
+})
+export class PriceDisplayComponent {
+  @Input() price: number;
+}
